6626c5b5b702ce674d7e7ee8de2045412b499d0b
sadness_ojisan <sadness.ojisan@gmail.com>
2022-02-26 18:45:48 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,72 @@
-extern crate combine;
-use combine::parser::char::{spaces, digit, char};
-use combine::{many1, sep_by, Parser, EasyParser};
-use combine::stream::easy;
+use combine::{
+    chainl1, choice,
+    error::ParseError,
+    parser::{
+        char::{char, digit, letter, spaces},
+    },
+    Parser, Stream, many1, between,
+};
+
+#[derive(Debug)]
+enum Expr {
+    Scalar(f64),
+    Var(char),
+    Prod(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+    Sum(Box<Expr>, Box<Expr>),
+    Diff(Box<Expr>, Box<Expr>),
+}
+
+fn parse_expr<Input>() -> impl Parser<Input, Output = Expr>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    let tok = choice([char('-'), char('+')]).map(|op| {
+        move |a, b| {
+            if op == '+' {
+                Expr::Sum(Box::new(a), Box::new(b))
+            } else if op == '-' {
+                Expr::Diff(Box::new(a), Box::new(b))
+            } else {
+                unimplemented!()
+            }
+        }
+    });
+    let mut sum = chainl1(parse_term(), tok);
+    sum
+}
+
+fn parse_term<Input>() -> impl Parser<Input, Output = Expr>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    let tok = choice([char('*'), char('/')]).map(|op| {
+        move |a, b| {
+            if op == '*' {
+                Expr::Prod(Box::new(a), Box::new(b))
+            } else if op == '/' {
+                Expr::Div(Box::new(a), Box::new(b))
+            } else {
+                unimplemented!()
+            }
+        }
+    });
+    let mut prod = chainl1(parse_factor(), tok);
+    prod
+}
+
+fn parse_factor<Input>() -> impl Parser<Input, Output = Expr>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    let scalar = many1(digit()).map(|t: String| Expr::Scalar(t.parse().unwrap()));
+    scalar
+}
 
 fn main() {
-    //Parse spaces first and use the with method to only keep the result of the next parser
-    let integer = spaces()
-        //parse a string of digits into an i32
-        .with(many1(digit()).map(|string: String| string.parse::<i32>().unwrap()));
-
-    //Parse integers separated by commas, skipping whitespace
-    let mut integer_list = sep_by(integer, spaces().skip(char(',')));
-
-    //Call parse with the input to execute the parser
-    let input = "1234, 45,78";
-    let result: Result<(Vec<i32>, &str), easy::ParseError<&str>> =
-        integer_list.easy_parse(input);
-    match result {
-        Ok((value, _remaining_input)) => println!("{:?}", value),
-        Err(err) => println!("{}", err)
-    }
+    let res = parse_expr().parse("1+2*3");
+    println!("{:?}", res);
 }
